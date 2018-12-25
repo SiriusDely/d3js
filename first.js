18d87e7be13e05480803e7c6ad46d4ff31276658
Sirius Dely <siriusdely@icloud.com>
2018-12-25 08:10:40 +0700

    use scaleLinear

--- first.js
+++ first.js
@@ -1,11 +1,29 @@
-var ratData = [400, 900, 300, 600];
+// var ratData = [ 400, 900, 300, 600 ];
+// var ratData = [ 40, 70, 60, 20, 40, 100, 60 ];
+var ratData = [ 40, 70, 60, 20, 40, 100, 60, 40, 70, 60, 20, 40, 100, 60 ];
+var ratData = [ 800, 700, 600 ];
+
+var width = 500, height = 160;
+var yLegendLeftWidth = 25;
+var xAxisLength = 200;
+var yAxisLength = 100;
 
 var svg = d3.select('body')
   .append('svg')
-  .attr('width', 500)
-  .attr('height', 160);
+  .attr('width', width)
+  .attr('height', height);
 
 function drawChart(dataArray) {
+
+  var dataLength = dataArray.length;
+  var maxValue = d3.max(ratData, function(d) {
+    return +d;
+  });
+  var w = xAxisLength / dataLength;
+
+  var yScale = d3.scaleLinear()
+    .domain([0, maxValue])
+    .range([0, yAxisLength]);
 
   // create a selection and bind data
   var rects = svg.selectAll('rect')
@@ -15,18 +33,19 @@ function drawChart(dataArray) {
   rects.enter()
     .append('rect')
     .attr('x', function(d, i) {
-      return (i + 1) * 25;
+      // Set x coord of rect using length of array
+      return i * (xAxisLength / dataLength) + yLegendLeftWidth;
     })
-    .attr('width', 15)
+    .attr('width', w - 1)
     .attr('fill', 'steelblue')
     // merge new elements with existing ones,
     // so everything below applies to all.
     .merge(rects)
     .attr('height', function(d) {
-      return d/10 * 1.5;
+      return yScale(d);
     })
     .attr('y', function(d) {
-      return 150 - d/10 * 1.5;
+      return height - yScale(d) - 75;
     });
 
   // remove any unused bars
@@ -41,19 +60,19 @@ function drawChart(dataArray) {
 
   svg
     .append('line')
-    .attr('x1', 25)
+    .attr('x1', yLegendLeftWidth)
     .attr('y1', 00)
-    .attr('x2', 25)
-    .attr('y2', 150)
+    .attr('x2', yLegendLeftWidth)
+    .attr('y2', yAxisLength)
     .attr('stroke-width', 2)
     .attr('stroke', 'black');
 
   svg
     .append('line')
-    .attr('x1', 25)
-    .attr('y1', 150)
-    .attr('x2', (1 + dataArray.length) * 25)
-    .attr('y2', 150)
+    .attr('x1', yLegendLeftWidth)
+    .attr('y1', yAxisLength)
+    .attr('x2', xAxisLength)
+    .attr('y2', yAxisLength)
     .attr('stroke-width', 2)
     .attr('stroke', 'black');
 
