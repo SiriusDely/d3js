b841f2972eec9797d4a9a387e1864c672718631e
Sirius Dely <siriusdely@icloud.com>
2018-12-27 09:17:04 +0700

    boston with slider

--- boston.js
+++ boston.js
@@ -1,3 +1,8 @@
+// Vars for Slider
+var inputValue = null;
+var months = [ 'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December' ];
+
 // Width and Height of the whole visualization
 var width = 700,
   height = 580;
@@ -33,3 +38,58 @@ g.selectAll('path')
 .attr('fill', '#ccc')
 .attr('stroke', '#333')
 .attr('d', geoPath);
+
+var rodents = svg.append('g');
+
+rodents.selectAll('path')
+  .data(rodents_json.features)
+  .enter()
+  .append('path')
+  .attr('fill', initialDate)
+  .attr('stroke', '#999')
+  .attr('class', 'incident')
+  .attr('d', geoPath)
+  .on('mouseover', function(d) {
+    d3.select('h2').text(d.properties.LOCATION_STREET_NAME);
+    d3.select(this).attr('class', 'incident hover');
+  })
+  .on('mouseout', function(d) {
+    d3.select('h2').text('');
+    d3.select(this).attr('class', 'incident');
+  })
+;
+
+// When the input range changes update the value
+d3.select('#timeslide').on('input', function() {
+  update(+this.value);
+});
+
+// Update the fill of each SVG of class 'incident' with value
+function update(value) {
+  inputValue = months[value];
+  document.getElementById('range').innerHTML = inputValue;
+  d3.selectAll('.incident')
+    .attr('fill', dateMatch);
+}
+
+function dateMatch(data, value) {
+  var d = new Date(data.properties.OPEN_DT);
+  var m = months[d.getMonth()];
+  if (inputValue == m) {
+    this.parentElement.appendChild(this);
+    return 'red';
+  } else {
+    return '#999';
+  }
+}
+
+function initialDate(d, i) {
+  var d = new Date(d.properties.OPEN_DT);
+  var m = months[d.getMonth()];
+  if (m == 'January') {
+    this.parentElement.appendChild(this);
+    return 'red';
+  } else {
+    return '#999';
+  }
+}
